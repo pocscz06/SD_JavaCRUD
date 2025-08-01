a8116216cee4bf4d7dded621ae51d72a4ec3d3fc
Kayla Thornton <kaylathornton55@icloud.com>
2025-08-01 15:22:49 -0400

    Updated EmployeeDAO and replaced placeholders with sql queries

--- src/EmployeeDAO.java
+++ src/EmployeeDAO.java
@@ -1,13 +1,30 @@
 public class EmployeeDAO {
-    // get employee info, ACTION REQUIRED: add default parameters for more complex searching
+    // get employee info, ACTION REQUIRED: set default values to specify how to get data
     public void getEmployee(String fName, String lName, int id) {
         // SELECT * FROM employees WHERE firstName = fName;
-        // SELECT * FROM employees WHERE firstName = lName;
+        // SELECT * FROM employees WHERE lastName = lName;
         // SELECT * FROM employees WHERE employeeID = id;
     }
 
-    // update employee data by entering the column to update and the value
-    public void updateEmployeeData(String column, String value, int id) {
+    // ACTION REQUIRED: use switch... case to specify how to update employee data
+    public void updateEmployeeData(String column, String value, int id) {        // column is the column users want to update
+        switch (column) {
+            case "first name":
+                column = "firstName";
+                break;
+            case "last name":
+                column = "lastName";
+                break;
+            case "salary":
+                // call updateEmployeeSalary() to update salary
+                break;
+            case "job":
+                column = "jobTitle";
+                break;
+            case "division":
+                column = "division";
+                break;
+        }
         // either this or multiple switch commands
         // UPDATE employees SET { column } = value WHERE employeeID = id;
         // return updated employee column: SELECT * FROM employees WHERE employeeID = id;
@@ -16,6 +33,7 @@ public class EmployeeDAO {
     // update employee salary
     public void updateEmployeeSalary(int id, double newSalary) {
         // UPDATE employees SET salary = newSalary WHERE employeeID = id;
+        // return updated employee column: SELECT * FROM employees WHERE employeeID = id;
     }
 
 }
--- src/ReportGenerator.java
+++ src/ReportGenerator.java
@@ -1,14 +1,18 @@
 public class ReportGenerator {
     // placeholders, will replace with SQL queries
     public void getSalary(int ID, double salary){
-        System.out.println("Salary for " + ID + " : " + salary);
+        // return SELECT salary FROM employees WHERE employeeID = id;
     }
 
-    public void getPayByJob(int ID, String job, double salary){
-        System.out.println("Salary for " + job + " : " + salary);
+    // get total pay for everyone with provided job title
+    public void getPayByJob(String job){
+        // return SELECT jobTitle, SUM(salary) FROM employees GROUP BY jobTitle;
+        // return SELECT SUM(salary) FROM employees WHERE jobTitle=job;
     }
-
-    public void getPayByDivision(int ID, String div, double salary){
-        System.out.println("Salary for " + div + " : " + salary);
+    
+    // get total pay for everyone with provided division
+    public void getPayByDivision(String div){
+        // return SELECT division, SUM(salary) FROM employees GROUP BY division;
+        // return SELECT SUM(salary) FROM employees WHERE division=div;
     }
 }
