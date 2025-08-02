c19649adef8a7028798b922eff5f069bd2a2cccd
pocscz06 <158521706+pocscz06@users.noreply.github.com>
2025-08-01 21:05:04 -0400

    fix: Make socialSecurity a string, overload constructor method (for fetching from database and creating new Employee), fixed getters, added setters

--- src/FullEmployee.java
+++ src/FullEmployee.java
@@ -1,62 +1,117 @@
 public class FullEmployee implements IEmployee {
+    private int employeeID;
     public String firstName;
     public String lastName;
-    private int socialSecurity;
+    private String socialSecurity;
     public double salary;
     public String jobTitle;
     public String division;
     public String status;
 
-    public FullEmployee(String fname, String lname, int ssn, double sal, String title, 
-    String div, String stat){
+    // Constructor used when creating a new Employee
+    public FullEmployee(String fname, String lname, String ssn, double sal, String title, String division) {
+        firstName = fname;
+        lastName = lname;
+        socialSecurity = ssn;
+        salary = sal;
+        jobTitle = title;
+        this.division = division;
+        status = "FT";
+    }
+
+    // Constructor used when loading an Employee from the database (has ID)
+    public FullEmployee(int id, String fname, String lname, String ssn, double sal, String title, 
+    String div){
+        employeeID = id;
         firstName = fname;
         lastName = lname;
         socialSecurity = ssn;
         salary = sal;
         jobTitle = title;
         division = div;
-        status = stat;
+        status = "FT";
     }
 
     // Getters and setters
     @Override
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    @Override
     public String getFirstName(){
-        return "Employee First Name: " + firstName;
+        return firstName;
     }
 
     @Override
     public String getLastName(){
-        return "Employee Last Name: " + lastName;
+        return lastName;
     }
 
     @Override
-    public String getSalary(){
-        return "Employee salary: " + salary;
+    public String getSSN() {
+        return socialSecurity;
+    }
+
+    @Override
+    public double getSalary(){
+        return salary;
     }
 
     @Override
     public String getJobTitle(){
-        return "Job: " + jobTitle;
+        return jobTitle;
     }
 
     @Override
     public String getDivision(){
-        return "Employee division: " + division;
+        return division;
     }
 
     @Override
     public String getStatus(){
-        return "Employee status: " + status;
+        return status;
     }
 
     @Override
     public void getFullInfo(){
         System.out.println("Displaying employee: " + firstName + " " + lastName);
+        System.out.println("ID: " + employeeID);
         System.out.println("SSN: ********* ");
-        System.out.println("Employee Status " + status);
+        System.out.println("Employee Status: " + status);
         System.out.println("Salary: " + salary);
         System.out.println("Title: " + jobTitle);
         System.out.println("Division: " + division);
         System.out.println(" ------------------------- ");
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    @Override
+    public void setDivision(String division) {
+        this.division = division;
+    }
+
+    @Override
+    public void setEmployeeID(int id) {
+        employeeID = id;
     }
 }
--- src/IEmployee.java
+++ src/IEmployee.java
@@ -1,9 +1,21 @@
 interface IEmployee{
+    public int getEmployeeID();
     public String getFirstName();
     public String getLastName();
-    public String getSalary();
+    public String getSSN();
+    public double getSalary();
     public String getJobTitle();
     public String getDivision();
     public String getStatus();
     public void getFullInfo();
+
+    public void setFirstName(String firstName);
+    public void setLastName(String lastName);
+    // Assume SSN will never change, so no need for an SSN mutator
+    public void setSalary(double salary);
+    public void setJobTitle(String jobTitle);
+    public void setDivision(String division);
+    public void setEmployeeID(int id); 
+    // EmployeeID won't ever actually be changed since it's the primary key
+    // This is just for instances of Employee that are created in the program; they won't have an employee ID
 }
