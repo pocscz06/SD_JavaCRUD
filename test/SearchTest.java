873d1f88e92c4403f7a3dd472af711349139937f
Amara Irobi <beaamara6@gmail.com>
2025-08-01 22:14:24 -0400

    Employee search using name and SSN to verify

--- test/SearchTest.java
+++ test/SearchTest.java
@@ -1,5 +1,29 @@
 package test;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SearchTest {
-    
+
+    @Test
+    public void searchByNameTest() throws Exception {
+        IEmployee emp1 = EmployeeDAO.searchByName("Chester", "Wester");
+        assertNotNull(emp1);
+        assertEquals("Chester", emp1.getFirstName());
+        assertEquals("Wester", emp1.getLastName());
+    }
+
+    @Test
+    public void searchBySSNTest() throws Exception {
+        IEmployee emp2 = EmployeeDAO.searchBySSN("433675049");
+        assertNotNull(emp2);
+        assertEquals("Anna", emp2.getFirstName());
+        assertEquals("Wise", emp2.getLastName());
+    }
+
+    @Test
+    public void searchByNameNotFoundTest() throws Exception {
+        IEmployee emp3 = EmployeeDAO.searchByName("Johnny", "Chad");
+        assertNull(emp3);
+    }
 }
